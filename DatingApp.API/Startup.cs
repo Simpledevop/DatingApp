52db5693f1bcf95ab0be9a57a0e206577aefaf95
SimpleDevop <SimpleDevop@outlook.com>
2019-02-04 22:52:05 +0000

    Added value component , added allows any CORS to webapi start.

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -30,6 +30,8 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>();
             
             services.AddMvc().AddNewtonsoftJson();
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,7 +48,7 @@ namespace DatingApp.API
             }
 
             //app.UseHttpsRedirection();
-
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseRouting(routes =>
             {
                 routes.MapApplication();
