521729ab6b6863141caaf87490cff988c4a738af
SimpleDevop <SimpleDevop@outlook.com>
2019-02-26 00:21:57 +0000

    NG Error Handling - Interceptor Provider for Custom Errors

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -1,21 +1,52 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
 import { throwError, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable()
-export class ErrorInterceptor implements HttpInterceptor{
-    // So we don't have to catch indvidual errors that might occur with an httpRequest
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-return next.handle(req).pipe(
-    catchError(error => {
-        if(error instanceOf HttpErrorResponse){
-            const applicationError = error.header.get('Application-Error');
-            if(applicationError){
-                return throwError(applicationError);
+export class ErrorInterceptor implements HttpInterceptor {
+  // So we don't have to catch indvidual errors that might occur with an httpRequest
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(catchError(error => {
+
+        if (error.status === 401) {
+            return throwError(error.statusText);
+        }
+        const applicationError = error.headers.get('Application-Error');
+        if (applicationError) {
+            console.error(applicationError);
+            return throwError(applicationError);
+        }
+        // To show user friendly errors in console...e.g, modal state error.. validation failed...length of field..or username exists...
+        // instead of drilling through the exception object in the console, this will be show the nice bits of the exception.
+        const serverError = error.error;
+        let modalStateErrors = '';
+        if (serverError && typeof serverError === 'object' ) {
+            for (const key in serverError) {
+                if (serverError[key]) {
+                    modalStateErrors += serverError[key] + '\n';
+                }
             }
         }
-    })
-)
-    }
+        return throwError(modalStateErrors || serverError || 'Server Error');
+    }));
+  }
 }
+
+// Ussually Provider is used for Dependency Injection of Services but also can be used to intercept Http Requests.
+// Tell the ng_module Provider an object with some options how to handle this specific provider. In this case
+// it's a type of Http_Interceptor which intercept http request and detects and custom handles any errors.
+export const ErrorInterceptorProvider = {
+    provide: HTTP_INTERCEPTORS,
+    useClass: ErrorInterceptor,
+    multi: true
+};
